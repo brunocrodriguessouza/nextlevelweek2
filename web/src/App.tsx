1f0ddf85163587b434a3372cf3344f917c212d6c
Bruno Souza <brunocrodriguessouza@gmail.com>
2020-08-07 02:28:31 -0300

    [02] Criação do componente Landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Next Level Week</h1>
-    </div>
+    <Landing />
   );
 }
 
